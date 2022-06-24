fee3474a87e090f05b3b099ba4ec82cc43b754e6
Extreme Electronics <102665314+ExtremeElectronics@users.noreply.github.com>
2022-06-24 16:51:31 +0100

    Latest

--- ide.c
+++ ide.c
@@ -15,6 +15,9 @@
  *
  *	You should have received a copy of the GNU General Public License
  *	along with IDE-emu.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 
+ *      Modified for SD and PiPico Derek Woodroffe 2022
  */
 
 #include <stdio.h>
